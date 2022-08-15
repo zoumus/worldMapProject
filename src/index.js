ee3f92a3e4ac18fa15822102faeca50a58a36bfc
zehour <zehourchaoui@gmail.com>
2022-08-15 16:00:59 -0700

    added slider and render some data

--- src/index.js
+++ src/index.js
@@ -1,20 +1,52 @@
 import * as d3 from "d3";
+import { csvFormatBody } from "d3";
 import * as topojson from "topojson";
 import createMap from "./scripts/map";
 import {fetchData} from "./scripts/util/apiUtil";
+import {renderSlider} from "./scripts/map.js"
 
 document.addEventListener("DOMContentLoaded", async function(){
     let data = await fetchData("../src/scripts/data.json");
-    // data = data.map(object => {
+    data = data
+    // .map(object => {
     //     let countryName = object["Country Name"];
     //     return {[countryName]: object}
     // })
-    console.log(data)
+    //console.log(Object.keys(data[0])[0] === 'Aruba')
+    //console.log(data[0]["Country Name"])
+    // console.log(data[0])
     
     // data.forEach (object => {
     //     console.log(`GDP in 2010: ${object['2010']}`,`Country Name: ${object['Country Name']}`)
     // })
+    const dataObject = data.reduce(
+        (obj, item) => {
+            let name = item["Country Name"]
+           return Object.assign(obj, { [name]: item })
+        
+        }, {});
+      
+      console.log(dataObject)
 
-
+    const show  =[];
     createMap();
+    let map = document.querySelector('.map').addEventListener('click', e => {
+        console.log(e.target.dataset.name);
+        console.log(e.target.dataset, "data when clicked")
+        // console.log(data, "static data")
+
+        let countryName = e.target.dataset.name;
+        let modal = document.getElementById("modal");
+        let countryNameHolder = document.getElementById('country-name')
+        let countryCodeHolder = document.getElementById('country-code')
+        let countryYearHolder = document.getElementById('country-year')
+
+        modal.style.color = 'white'
+        countryNameHolder.innerHTML = dataObject[countryName]['Country Name']
+        countryCodeHolder.innerHTML = dataObject[countryName]['Country Code']
+
+        // body.appendChild(modal)
+    })
+    // console.log(map);
+    renderSlider()
 })
--- src/scripts/map.js
+++ src/scripts/map.js
@@ -6,45 +6,52 @@ const createMap = function(){
     const width = 900;
     const height = 600;
 
-    const svg = d3.select('body').append('svg').attr('width', width).attr('height', height);
-
+    const svg = d3.select('body').append('svg').attr('width', width).attr('height', height).attr('class', 'map');
+    
     const projection = d3.geoMercator().scale(120)
         .translate([width / 2, height / 1.4]);
     const path = d3.geoPath(projection);
 
-    const g = svg.append('g');
+    // const g = svg.append('g');
     console.log(svg)
 
     d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
         .then(data => {
-
+            // console.log(data);
             const countries = topojson.feature(data, data.objects.countries);
-            g.selectAll('path').data(countries.features).enter().append('path').attr('class', 'country').attr('d', path);
-
+            svg.selectAll('path').data(countries.features).enter().append('path').attr('class', 'country').attr('d', path).attr('data-name', ele => ele.properties.name);
         });
 }
-// function renderSlider() {
-//     const slider = document.createElement("div");
-//     slider.setAttribute("id", "slider-container");
 
-//     const sliderInput = document.createElement("input");
-//     sliderInput.setAttribute("id", "year-slider");
-//     sliderInput.setAttribute("type", "range");
-//     sliderInput.setAttribute("min", "2005");
-//     sliderInput.setAttribute("max", "2020");
-//     sliderInput.setAttribute("value", "2018");
-//     sliderInput.setAttribute("step", "1");
+// consider adding hover funcionality in the js file instead of css
+// in the hover event listerner, 
+// display tooltip
+// change color
 
-//     const sliderLabel = document.createElement("span");
-//     sliderLabel.setAttribute("id", "slider-current-year");
-//     sliderLabel.innerHTML = 2018;
+// separate funciton for creating toolitp when path is created
+
+export function renderSlider() {
+    const slider = document.createElement("div");
+    slider.setAttribute("id", "slider-container");
+
+    const sliderInput = document.createElement("input");
+    sliderInput.setAttribute("id", "year-slider");
+    sliderInput.setAttribute("type", "range");
+    sliderInput.setAttribute("min", "2005");
+    sliderInput.setAttribute("max", "2020");
+    sliderInput.setAttribute("value", "2020");
+    sliderInput.setAttribute("step", "1");
+
+    const sliderLabel = document.createElement("span");
+    sliderLabel.setAttribute("id", "slider-current-year");
+    sliderLabel.innerHTML = 2020;
     
-//     slider.appendChild(sliderInput);
-//     slider.appendChild(sliderLabel);
-//     document.getElementsByClassName("us-map-container")[0].appendChild(slider);
+    slider.appendChild(sliderInput);
+    slider.appendChild(sliderLabel);
+    document.getElementsByClassName("title-years")[0].appendChild(slider);
 
-//     document.getElementById("slider-current-year").style.left = `calc( 100% - 12.5px - ${document.getElementById("slider-current-year").offsetWidth / 2}px)`;
-// }
+    document.getElementById("slider-current-year").style.left = `calc( 100% - 12.5px - ${document.getElementById("slider-current-year").offsetWidth / 2}px)`;
+}
 
 export default createMap;
     
