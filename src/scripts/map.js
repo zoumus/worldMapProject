390995e975845e0d63858a2a55592f69f7f8befa
zehour <zehourchaoui@gmail.com>
2022-08-14 21:35:09 -0700

    added some html

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -2,12 +2,13 @@ import * as d3 from "d3";
 import * as topojson from "topojson";
 
 const createMap = function(){
+
     const width = 900;
     const height = 600;
 
     const svg = d3.select('body').append('svg').attr('width', width).attr('height', height);
 
-    const projection = d3.geoMercator().scale(140)
+    const projection = d3.geoMercator().scale(120)
         .translate([width / 2, height / 1.4]);
     const path = d3.geoPath(projection);
 
@@ -21,8 +22,29 @@ const createMap = function(){
             g.selectAll('path').data(countries.features).enter().append('path').attr('class', 'country').attr('d', path);
 
         });
-    
 }
+// function renderSlider() {
+//     const slider = document.createElement("div");
+//     slider.setAttribute("id", "slider-container");
+
+//     const sliderInput = document.createElement("input");
+//     sliderInput.setAttribute("id", "year-slider");
+//     sliderInput.setAttribute("type", "range");
+//     sliderInput.setAttribute("min", "2005");
+//     sliderInput.setAttribute("max", "2020");
+//     sliderInput.setAttribute("value", "2018");
+//     sliderInput.setAttribute("step", "1");
+
+//     const sliderLabel = document.createElement("span");
+//     sliderLabel.setAttribute("id", "slider-current-year");
+//     sliderLabel.innerHTML = 2018;
+    
+//     slider.appendChild(sliderInput);
+//     slider.appendChild(sliderLabel);
+//     document.getElementsByClassName("us-map-container")[0].appendChild(slider);
+
+//     document.getElementById("slider-current-year").style.left = `calc( 100% - 12.5px - ${document.getElementById("slider-current-year").offsetWidth / 2}px)`;
+// }
 
 export default createMap;
     
